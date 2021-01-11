3f961f793f6996a69e20bd464863240697923cae
Rebeca Nicole Sandoval Zambrano <75395340+Rebeca0710@users.noreply.github.com>
2021-01-11 15:40:09 -0500

    Add files via upload

---
+++ workshop/unit1/WS01HelloWorld/HelloWorld.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+int main(){
+	int grade=20
+	//printf() displays information
+	printf("Hello Wolrd from \t \" Rebeca Sandoval! \" =) \n");
+	printf("\n The grade is %d \n", grade);
+	return 0;
+}
+
+	
