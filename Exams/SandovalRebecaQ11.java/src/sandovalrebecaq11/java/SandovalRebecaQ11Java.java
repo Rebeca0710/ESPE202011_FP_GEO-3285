5fcc695cde33c57426448cd82da454ec8a2311a6
Rebeca Nicole Sandoval Zambrano <75395340+Rebeca0710@users.noreply.github.com>
2021-01-18 19:02:38 -0500

    Add files via upload

---
+++ Exams/SandovalRebecaQ11.java/src/sandovalrebecaq11/java/SandovalRebecaQ11Java.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sandovalrebecaq11.java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Augusto
+ */
+public class SandovalRebecaQ11Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    boolean mainLoop = true;
+    int option = 0;
+    int i;
+    i = 0;
+    
+    do{
+        System.out.println("Enter the table that you want");
+        System.out.println("Multiplication Tables : 6");       
+        
+            
+                int top = 12;
+                int product=0;
+                int table = 6;
+                for (int j = 1 ; j <= top ; j++){
+                product = table * j;
+                System.out.println("6 * " + j + " = " + product);
+          }
+                {
+                    
+        System.out.println("Enter the table that you want ->0");
+                System.exit(0);              
+    
+                                                                                                                                                  
+    }}while (option < 6);
+        
+                                                                             
+       }       
+    } 
+    
+
